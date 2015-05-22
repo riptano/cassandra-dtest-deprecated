f0ea3ae92aef3228a713997bb40525865f0e0e4f
Shawn Kumar <shawn.kumar@datastax.com>
2015-05-22 17:53:06 -0400

    fixes + offlinerelevel test

--- offline_tools_test.py
+++ offline_tools_test.py
@@ -8,52 +8,60 @@ import re
 class TestOfflineTools(Tester):
 
     def sstablelevelreset_test(self):
-    """
-    Insert data and call sstablelevelreset on a series of 
-    tables. Confirm level is reset and that compaction works
-    normally using sstable2json to read tables.
-    @since 2.1.5
-    """
+        """
+        Insert data and call sstablelevelreset on a series of 
+        tables. Confirm level is reset using sstable2json to read tables.
+        Test a variety of possible errors and ensure response is resonable.
+        @since 2.1.5
+        @jira CASSANDRA-7614
+        """
         cluster = self.cluster
         cluster.populate(3).start()
         [node1,node2, node3] = cluster.nodelist()
 
         #test by trying to run on nonexistent keyspace
-        cluster.stop()
-        (output, rc) = node1.run_sstablelevelreset("keyspace1", "standard1", output=True)
-        self.assertTrue("ColumnFamily not found: keyspace1/standard1" in output, msg="Did not throw CF not found error")
-        self.assertEqual(rc, 0, msg="Return code was not 0 where should have failed")
+        cluster.stop(gently=False)
+        (output, error, rc) = node1.run_sstablelevelreset("keyspace1", "standard1", output=True)
+        self.assertTrue("ColumnFamily not found: keyspace1/standard1" in error, msg="Did not throw CF not found error")
+        # this should return exit code 0!
+        self.assertEqual(rc, 1, msg="Return code was not 1 with nonexistent keyspace.")
 
         #now test by generating keyspace but not flushing sstables
         cluster.start()
         node1.stress(['write', 'n=100', '-schema', 'replication(factor=3)'])
-        cursor = self.patient_cql_connection(node1)
-        cursor.execute("ALTER TABLE keyspace1.standard1 with compaction={'class': 'LeveledCompactionStrategy'};")
         cluster.stop()
 
-        (output, rc) = node1.run_sstablelevelreset("keyspace1", "standard1", output=True)
+        (output, error, rc) = node1.run_sstablelevelreset("keyspace1", "standard1", output=True)
+
         self.assertTrue("Found no sstables, did you give the correct keyspace/columnfamily?" in output, msg="Improper error msg with no sstables")
-        self.assertEqual(rc, 0, msg="improper return code when no sstables")
+        self.assertEqual(rc, 0, msg="improper return code was not 1 with no sstables")
 
         #test by writing small amount of data and flushing (all sstables should be level 0)
-        cluster.start()
-        node1.stress(['write', 'n=100000', '-schema', 'replication(factor=3)'])
+        cluster.start() 
+        cursor = self.patient_cql_connection(node1)
+        cursor.execute("ALTER TABLE keyspace1.standard1 with compaction={'class': 'LeveledCompactionStrategy'};")
+        node1.stress(['write', 'n=1000000', '-schema', 'replication(factor=3)'])
         node1.flush()
         cluster.stop()
 
-        (output, rc) = node1.run_sstablelevelreset("keyspace1", "standard1", output=True)
+        (output, error, rc) = node1.run_sstablelevelreset("keyspace1", "standard1", output=True)
         self.assertTrue("since it is already on level 0" in output, msg="Did not skip sstables where level = 0")
-        self.assertEqual(rc, 0, msg="improper return code where level = 0")
+        self.assertEqual(rc, 0, msg="improper return code of 1 where level = 0")
 
         #test by loading large amount data so we have multiple levels and checking all levels are 0 at end
         cluster.start()
         node1.stress(['write', 'n=5000000', '-schema', 'replication(factor=3)'])
         node1.flush()
+        node1.stress(['write', 'n=5000000', '-schema', 'replication(factor=3)'])
+        node1.flush()
         cluster.stop()
 
-        initial_levels = self.get_levels(node1.run_sstablemetadata(keyspace="keyspace1", column_families="standard1"))
-        (output, rc) = node1.run_sstablelevelreset("keyspace1", "standard1", output=True)
-        final_levels = self.get_levels(node1.run_sstablemetadata(keyspace="keyspace1", column_families="standard1"))
+        initial_levels = self.get_levels(node1.run_sstablemetadata(keyspace="keyspace1", column_families=["standard1"]))
+        (output, error, rc) = node1.run_sstablelevelreset("keyspace1", "standard1", output=True)
+        final_levels = self.get_levels(node1.run_sstablemetadata(keyspace="keyspace1", column_families=["standard1"]))
+
+        debug(initial_levels)
+        debug(final_levels)
 
         for x in range(0, len(final_levels)):
             initial = "intial level: " + str(initial_levels[x])
@@ -62,14 +70,64 @@ class TestOfflineTools(Tester):
     def get_levels(self, data):
         levels = []
         for sstable in data:
-            (metadata, rc) = sstable
+            (metadata, error, rc) = sstable
             level = int(re.findall("SSTable Level: [0-9]", metadata)[0][-1])
             levels.append(level)
         return levels
 
     def sstableofflinerelevel_test(self):
-    """ 
-    Generate sstables and allow compaction to run.
-    Stop node and run sstableofflinerelevel. 
-    """
-        pass
+        """ 
+        Generate sstables and allow compaction to run.
+        Stop node and return sstableofflinerelevel. 
+        @since 2.1.5
+        @jira CASSANRDA-8031
+        """
+        cluster = self.cluster      
+        cluster.populate(3).start()
+        [node1,node2, node3] = cluster.nodelist()
+
+        #test by trying to run on nonexistent keyspace
+        cluster.stop(gently=False)
+        (output, error, rc) = node1.run_sstableofflinerelevel("keyspace1", "standard1", output=True)
+        self.assertTrue("java.lang.IllegalArgumentException: Unknown keyspace/columnFamily keyspace1.standard1" in error)
+        # this should return exit code 0!
+        self.assertEqual(rc, 1, msg="Return code was not 1 with nonexistent keyspace.")
+
+        #now test by generating keyspace but not flushing sstables
+        cluster.start()
+        node1.stress(['write', 'n=100', '-schema', 'replication(factor=3)'])
+        cluster.stop()
+
+        (output, error, rc) = node1.run_sstableofflinerelevel("keyspace1", "standard1", output=True)
+
+        self.assertTrue("No sstables to relevel for keyspace1.standard1" in output, msg="Improper error msg with no sstables")
+        self.assertEqual(rc, 1, msg="Improper return code was not 1 with no sstables")
+
+        #test by flushing (sstable should be level 0)
+        cluster.start() 
+        cursor = self.patient_cql_connection(node1)
+        cursor.execute("ALTER TABLE keyspace1.standard1 with compaction={'class': 'LeveledCompactionStrategy'};")
+        node1.flush()
+        cluster.stop()
+
+        (output, error, rc) = node1.run_sstableofflinerelevel("keyspace1", "standard1", output=True)
+        self.assertTrue("L0=1" in output)
+        self.assertEqual(rc, 0, msg="improper return code of 1.")
+
+        #test by loading large amount data so we have multiple levels
+        cluster.start()
+        node1.stress(['write', 'n=5000000', '-schema', 'replication(factor=3)'])
+        node1.flush()
+        cluster.stop()
+
+        #check that levels are greater than or equal to previous
+        initial_levels = self.get_levels(node1.run_sstablemetadata(keyspace="keyspace1", column_families="standard1"))
+        (output, error, rc) = node1.run_sstableofflinerelevel("keyspace1", "standard1", output=True)
+        final_levels = self.get_levels(node1.run_sstablemetadata(keyspace="keyspace1", column_families="standard1"))
+
+        debug(initial_levels)
+        debug(final_levels)
+        debug(output)
+
+        for x in range(0, len(final_levels)):
+            self.assertGreaterEqual(final_levels[x], initial_level[x])
