69b861751c2994f257433c7fe8e41cb3eca4eb3c
Craig Kodman <ckoddrums@gmail.com>
2016-08-05 14:08:34 -0400

    fix search calls

--- jmx_test.py
+++ jmx_test.py
@@ -130,8 +130,9 @@ class TestJMX(Tester):
             time.sleep(2)
 
             updated_progress_string = jmx.read_attribute(compaction_manager, 'CompactionSummary')[0]
-            progress = int(parse.search('Compaction@{uuid}(keyspace1, standard1, {progress}/{total})bytes', progress_string).named['progress'])
-            updated_progress = int(parse.search('Compaction@{uuid}(keyspace1, standard1, {updated_progress}/{updated_total})bytes', updated_progress_string).named['updated_progress'])
+            var = 'Compaction@{uuid}(keyspace1, standard1, {progress}/{total})bytes'
+            progress = int(parse.search(var, progress_string).named['progress'])
+            updated_progress = int(parse.search(var, updated_progress_string).named['progress']
 
             debug(progress_string)
             debug(updated_progress_string)
@@ -146,8 +147,8 @@ class TestJMX(Tester):
             # Otherwise nodetool will throw an exception
             # Give a timeout, in case compaction is broken
             # and never ends.
-            start = time.time()
-            max_query_timeout = 600
+            start=time.time()
+            max_query_timeout=600
             debug("Waiting for compaction to finish:")
             while (len(jmx.read_attribute(compaction_manager, 'CompactionSummary')) > 0) and (time.time() - start < max_query_timeout):
                 debug(jmx.read_attribute(compaction_manager, 'CompactionSummary'))
@@ -164,23 +165,23 @@ class TestJMX(Tester):
         @jira_ticket CASSANDRA-9526
         """
 
-        cluster = self.cluster
+        cluster=self.cluster
         cluster.populate(3).start(wait_for_binary_proto=True)
-        node1, node2, node3 = cluster.nodelist()
+        node1, node2, node3=cluster.nodelist()
 
         phivalues = node1.nodetool("failuredetector").stdout.splitlines()
         endpoint1Values = phivalues[1].split()
         endpoint2Values = phivalues[2].split()
 
-        endpoint1 = endpoint1Values[0][1:-1]
-        endpoint2 = endpoint2Values[0][1:-1]
+        endpoint1=endpoint1Values[0][1:-1]
+        endpoint2=endpoint2Values[0][1:-1]
 
         self.assertItemsEqual([endpoint1, endpoint2], ['127.0.0.2', '127.0.0.3'])
 
-        endpoint1Phi = float(endpoint1Values[1])
-        endpoint2Phi = float(endpoint2Values[1])
+        endpoint1Phi=float(endpoint1Values[1])
+        endpoint2Phi=float(endpoint2Values[1])
 
-        max_phi = 2.0
+        max_phi=2.0
         self.assertGreater(endpoint1Phi, 0.0)
         self.assertLess(endpoint1Phi, max_phi)
 
@@ -191,8 +192,8 @@ class TestJMX(Tester):
 @since('3.9')
 class TestJMXSSL(Tester):
 
-    keystore_password = 'cassandra'
-    truststore_password = 'cassandra'
+    keystore_password='cassandra'
+    truststore_password='cassandra'
 
     def truststore(self):
         return os.path.join(self.test_path, 'truststore.jks')
@@ -205,8 +206,8 @@ class TestJMXSSL(Tester):
         Check connecting with a JMX client (via nodetool) where SSL is enabled for JMX
         @jira_ticket CASSANDRA-12109
         """
-        cluster = self._populateCluster(require_client_auth=False)
-        node = cluster.nodelist()[0]
+        cluster=self._populateCluster(require_client_auth=False)
+        node=cluster.nodelist()[0]
         cluster.start()
 
         self.assert_insecure_connection_rejected(node)
@@ -220,8 +221,8 @@ class TestJMXSSL(Tester):
         client certificate auth is also configured
         @jira_ticket CASSANDRA-12109
         """
-        cluster = self._populateCluster(require_client_auth=True)
-        node = cluster.nodelist()[0]
+        cluster=self._populateCluster(require_client_auth=True)
+        node=cluster.nodelist()[0]
         cluster.start()
 
         self.assert_insecure_connection_rejected(node)
@@ -243,16 +244,16 @@ class TestJMXSSL(Tester):
             node.nodetool("info")
 
     def _populateCluster(self, require_client_auth=False):
-        cluster = self.cluster
+        cluster=self.cluster
         cluster.populate(1)
 
         generate_ssl_stores(self.test_path)
         if require_client_auth:
-            ts = self.truststore()
-            ts_pwd = self.truststore_password
+            ts=self.truststore()
+            ts_pwd=self.truststore_password
         else:
-            ts = None
-            ts_pwd = None
+            ts=None
+            ts_pwd=None
 
         enable_jmx_ssl(cluster.nodelist()[0],
                        require_client_auth=require_client_auth,
