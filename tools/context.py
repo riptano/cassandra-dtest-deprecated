05ce2af0844a079afd44baf7a58c9cf95a3115d0
Russ Hatch <russthehatch@gmail.com>
2016-09-01 11:01:51 -0600

    move from class-based context manager to @contextmanager function

--- tools/context.py
+++ tools/context.py
@@ -4,70 +4,66 @@ making those context managers function.
 """
 import logging
 import os
+from contextlib import contextmanager
 
 from six import print_
 
 ALLOW_NOISY_LOGGING = os.environ.get('ALLOW_NOISY_LOGGING', '').lower() in ('yes', 'true')
 
 
-class silencing_of(object):
+@contextmanager
+def silencing_of(log_id, expected_strings=None):
     """
     Context manager which allows silencing logs until exit.
-
     Log records matching expected_strings will be filtered out of logging.
-
     If expected_strings is not provided, everything is filtered for that log.
     """
-    def __init__(self, log_id, expected_strings=None):
-        self.logger = logging.getLogger(log_id)
-        self._filter = self._get_filter_obj(expected_strings)
+    logger = logging.getLogger(log_id)
+    log_filter = _make_filter_class(expected_strings)
+    logger.addFilter(log_filter)
+    yield
+    if log_filter.records_silenced > 0:
+            print_("Logs were filtered to remove messages deemed unimportant, total count: {}".format(log_filter.records_silenced))
+    logger.removeFilter(log_filter)
 
-    def __enter__(self):
-        self.logger.addFilter(self._filter)
 
-    def __exit__(self, exc_type, exc_value, traceback):
-        if self._filter.records_silenced > 0:
-            print_("Logs were filtered to remove messages deemed unimportant, total count: {}".format(self._filter.records_silenced))
+def _make_filter_class(expected_strings):
+    """
+    Builds an anon-ish filtering class and returns it.
 
-        self.logger.removeFilter(self._filter)
+    Returns a logfilter if filtering should take place, otherwise a nooplogfilter.
 
-    def _get_filter_obj(self, expected_strings):
-        """
-        Builds an anon-ish filtering class and returns it.
+    We're just using a class here as a one-off object with a filter method, for
+    use as a filter object on the desired log.
+    """
+    class nooplogfilter(object):
+        records_silenced = 0
 
-        Returns a logfilter if filtering should take place, otherwise a nooplogfilter.
+        @staticmethod
+        def filter(record):
+            return True
 
-        We're just using a class here as a one-off object with a filter method, for
-        use as a filter object on the desired log.
-        """
-        class nooplogfilter(object):
-            records_silenced = 0
+    class logfilter(object):
+        records_silenced = 0
 
-            @staticmethod
-            def filter(record):
-                return True
+        @classmethod
+        def increment_filtered(cls):
+            cls.records_silenced += 1
 
-        class logfilter(object):
-            records_silenced = 0
+        @staticmethod
+        def filter(record):
+            if expected_strings is None:
+                logfilter.increment_filtered()
+                return False
 
-            @classmethod
-            def increment_filtered(cls):
-                cls.records_silenced += 1
-
-            @staticmethod
-            def filter(record):
-                if expected_strings is None:
+            for s in expected_strings:
+                if s in record.msg or s in record.name:
                     logfilter.increment_filtered()
                     return False
 
-                for s in expected_strings:
-                    if s in record.msg or s in record.name:
-                        logfilter.increment_filtered()
-                        return False
+            return True
 
-                return True
-
-        if ALLOW_NOISY_LOGGING:
-            return nooplogfilter
-        else:
-            return logfilter
+    if ALLOW_NOISY_LOGGING:
+        return nooplogfilter
+    else:
+        return logfilter
